fb3334b03aca17234e1a8ecdc4ac878465cf3f99
Christian Lameda <christianlameda@gmail.com>
2023-04-21 17:34:40 +0200

    challenge finished

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-#imports
-
-
-def main():
-  
+from modules import module
 
 if __name__ == '__main__':
-    main()
+  
+    DF_PULLS = get_pulls(BASE_URL, KEY, OWNER, REPO, PULLS, SEARCH, STATE, USERNAME, API_TOKEN, field_list1)
+    DF_STATUS = df_status(DF_PULLS, BASE_URL, KEY, OWNER, REPO, COMMITS, USERNAME, API_TOKEN, field_list2)
+    DF_CSV = create_csv(DF_STATUS, field_sort1, field_name1)
